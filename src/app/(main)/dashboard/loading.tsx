02adb426ffeb9814371614c436e4486b0eb2325e
Rafi Ahmad <rafiahmad05111@gmail.com>
2025-09-01 03:45:24 +0800

    add loading screen

---
+++ src/app/(main)/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="custom-loader"></div>
+    </div>
+  )
+}
+
+export default Loading
